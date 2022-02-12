a3279acf1a730e739c62a04717d4645a11e23bcc
stephen <josephraj1608@gmai.com>
2022-02-12 23:19:19 +0530

    sample

---
+++ SampleWebApp/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace SampleWebApp.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Customer (int pId, string pName) {
+            Id = pId;   
+            Name = pName;
+        }
+    }
+}
